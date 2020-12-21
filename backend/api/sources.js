b4470b11c6104f7d61e2e38acfc78f4795569922
Furkan Mutlu <furkanmutluu@gmail.com>
2020-12-21 13:41:51 +0100

    Deploy

--- backend/api/sources.js
+++ backend/api/sources.js
@@ -1,12 +1,12 @@
 const apiSources = {
     giphy: {
-        key: 'gTRJN6wC3Jh4PXbDZdisknRrVnv8xWST',
+        key: process.env.GIPHY_API_KEY, // Your Giphy API Key here
         link: 'http://api.giphy.com/v1/gifs/search?api_key=',
         trendingLink: 'http://api.giphy.com/v1/gifs/trending?api_key=',
         limitString: 'limit'
     },
     pixabay: {
-        key: '18743317-528f2b58f97457880e48dbd06',
+        key: process.env.PIXABAY_API_KEY, // Your Pixabay API Key here
         link: 'https://pixabay.com/api/?key=',
         trendingLink: 'https://pixabay.com/api/?key=',
         limitString: 'per_page'

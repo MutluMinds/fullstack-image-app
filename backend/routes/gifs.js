773c59d76266b7c093c653e54729b3b1df4e7b00
Furkan Mutlu <furkanmutluu@gmail.com>
2020-12-21 11:24:41 +0100

    Added: Error handling

--- backend/routes/gifs.js
+++ backend/routes/gifs.js
@@ -2,12 +2,23 @@ const router = require('express').Router();
 const { getTrendingData, getSearchedImages } = require('../services/API');
 
 router.route('/').get(async (req, res) => {
-    res.json(await getTrendingData('gifs', 15));
+    try {
+        res.json(await getTrendingData('gifs', 15));
+    } catch (error) {
+        res.status(404);
+        console.log(error);
+    }
 });
 
 router.route('/search').get(async (req, res) => {
     const searchTerm = req.query.searchTerm;
-    res.json(await getSearchedImages('gif', 15, searchTerm));
+
+    try {
+        res.json(await getSearchedImages('gif', 15, searchTerm));
+    } catch (error) {
+        res.status(404);
+        console.log(error);
+    }
 });
 
 module.exports = router;

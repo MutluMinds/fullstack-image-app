f105448b3d50b6381e9c4c04d1819ee8f167c6d2
Rizwan Rabbani <33807776+rabbanirizwan@users.noreply.github.com>
2022-11-30 00:26:08 +0500

    make searchbar sticky (#21)
    * make searchbar sticky
    
    * resolve issues
    
    * few improvements
    
    Co-authored-by: Furkan <furkanmutluu@gmail.com>

--- frontend/src/components/SearchImage/SearchImage.jsx
+++ frontend/src/components/SearchImage/SearchImage.jsx
@@ -1,28 +1,48 @@
-import React, { useRef } from 'react'
-import './SearchImage.scss';
+import React, { useRef, useState, useEffect } from "react";
+import "./SearchImage.scss";
+
+const SCROLL_TOP_Y = 20;
 
 const SearchImage = ({ onChange, setInputValue, value }) => {
-    const searchRef = useRef();
+  const searchRef = useRef();
+  const [isSearchBarFull, setIsSearchBarFull] = useState(false);
 
-    function handleSearchSubmit(e) {
-        e.preventDefault();
-        const searchTerm = searchRef.current.value;
-        onChange(searchTerm);
-    }
+  function handleSearchSubmit(e) {
+    e.preventDefault();
+    const searchTerm = searchRef.current.value;
+    onChange(searchTerm);
+  }
 
-    return (
-        <div className="search_input-wrapper">
-            <form onSubmit={handleSearchSubmit}>
-                <input
-                    ref={searchRef}
-                    value={value}
-                    onChange={e => setInputValue(e.target.value)}
-                    className="search_input"
-                    type="text"
-                    placeholder="Search..." />
-            </form>
-        </div>
-    )
-}
+  const isSticky = () => {
+    const windownTopY = window.scrollY;
+    setIsSearchBarFull(windownTopY > SCROLL_TOP_Y);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isSticky);
+
+    return () => {
+      window.removeEventListener("scroll", isSticky);
+    };
+  }, []);
+
+  return (
+    <div className="search_input-wrapper">
+      <form
+        onSubmit={handleSearchSubmit}
+        className={isSearchBarFull ? 'search-bar-lg' : 'search-bar-md'}
+      >
+        <input
+          ref={searchRef}
+          value={value}
+          onChange={e => setInputValue(e.target.value)}
+          className="search_input"
+          type="text"
+          placeholder="Search..."
+        />
+      </form>
+    </div>
+  );
+};
 
 export default SearchImage;

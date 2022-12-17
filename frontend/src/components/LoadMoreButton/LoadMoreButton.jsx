ad9c38d46e1e781d31ce70fe32e08dace810ff52
Elyes Arifa <49849558+elyessAR@users.noreply.github.com>
2022-12-17 19:15:02 +0100

    Auto load is enabled when scroller hit the bottom of the page (Fixes Issue #34) (#35)
    * When the scroller comes closer to the buttom LoadMoreImages triggers
    
    * I guess this is a first timer mistake, I shouldn't edit this here.
    
    * Bug fix: handle with offset and multiloads
    
    * Add listener for once
    
    * Fix linting issue
    
    Co-authored-by: Elyes Arifa <earifa@resqsoft.com>
    Co-authored-by: Furkan <furkanmutluu@gmail.com>

--- frontend/src/components/LoadMoreButton/LoadMoreButton.jsx
+++ frontend/src/components/LoadMoreButton/LoadMoreButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
@@ -13,22 +13,40 @@ const LoadMoreButton = ({
   setImages,
 }) => {
   const [isLoading, setIsLoading] = useState(false);
+  const loadingRef = useRef();
 
-  const loadMoreImages = async () => {
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const loadMoreImages = async (loadOffset) => {
     setIsLoading(true);
 
-    getImages(imageType, offset, inputValue).then((receivedImages) => {
-      const newOffset = getNewOffset(imageType, offset);
+    getImages(imageType, loadOffset, inputValue).then((receivedImages) => {
+      const newOffset = getNewOffset(imageType, loadOffset);
       setImages((images) => [...images, ...receivedImages]);
       setOffset(newOffset);
       setIsLoading(false);
+      loadingRef.current = false;
     });
+  };
+  const handleScroll = () => {
+    if (!loadingRef.current && window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      setOffset(currentOffset => {
+        loadingRef.current = true;
+        loadMoreImages(currentOffset);
+        return currentOffset;
+      });
+    }
   };
 
   return (
     <div className="moreImages">
       {!isLoading ? (
-        <button className="moreImagesButton" onClick={loadMoreImages}>
+        <button className="moreImagesButton" onClick={() => loadMoreImages(offset)}>
           <FontAwesomeIcon
             style={{ color: "white", fontSize: "32px" }}
             icon={faAngleDown}

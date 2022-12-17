a8474afc7e5ff0394a5df40f615b2e046b9d52b2
Furkan <furkanmutluu@gmail.com>
2022-12-17 22:02:27 +0100

    [BUG] Search value got reset #37

--- frontend/src/components/StickyBar/StickyBar.jsx
+++ frontend/src/components/StickyBar/StickyBar.jsx
@@ -1,38 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { getDefaultOffset, getImages, getNewOffset } from "../../utils";
 
 import SearchImage from "../SearchImage/SearchImage";
 import ScrollButton from "../ScrollTopButton/ScrollTopButton";
 
 const SCROLL_TOP_Y = 30;
 
-const StickyBar = ({
-  imageType,
-  setIsLoading,
-  setInputValue,
-  setImages,
-  setOffset,
-  inputValue
-}) => {
+const StickyBar = ({ setInputValue }) => {
   const [sticky, setSticky] = useState(false);
-
-  async function switchImagesProvider(searchTerm) {
-    setIsLoading(true);
-    setInputValue(searchTerm);
-
-    const defaultOffset = getDefaultOffset(imageType);
-    getImages(imageType, defaultOffset, searchTerm).then((receivedImages) => {
-      const newOffset = getNewOffset(imageType, defaultOffset);
-      setImages(receivedImages);
-      setOffset(newOffset);
-      setIsLoading(false);
-    });
-  }
 
   const isSticky = () => {
     const windownTopY = window.scrollY;
     setSticky(windownTopY > SCROLL_TOP_Y);
-  }
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", isSticky);
@@ -44,11 +23,7 @@ const StickyBar = ({
 
   return (
     <div className="sticky-wrapper">
-      <SearchImage
-        onChange={(searchTerm) => switchImagesProvider(searchTerm)}
-        value={inputValue}
-        setInputValue={setInputValue}
-      />
+      <SearchImage onSearch={(searchTerm) => setInputValue(searchTerm)} />
       {sticky ? <ScrollButton /> : null}
     </div>
   );
